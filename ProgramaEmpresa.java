3d55e795eb17a599f44bed5330a31eab7b76a0c5
brunoyep <65029687+brunoyep@users.noreply.github.com>
2020-05-11 17:44:03 -0300

    Update ProgramaEmpresa.java

--- ProgramaEmpresa.java
+++ ProgramaEmpresa.java
@@ -1,8 +1,67 @@
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.Scanner;
+import java.util.List;
 
 public class ProgramaEmpresa {
 
     public static void main(String[] args) throws Exception {
+        
+        List<Empresa> listEmpresa = new ArrayList<Empresa>();
+
+        Empresa emp = new Empresa();
+        emp.setNome("minha empresa");
+        emp.setFaturamento(10000000);
+        
+        listaEmpresa.add(emp);
+        
+        Empresa empresa1 = new Empresa();
+        empresa1.setNome("Empresa Bancaria");
+        empresa1.setCnpj("12.333.333/0001-99");
+        Scanner leitor = new Scanner(System.in);
+
+        int opcao;
+
+        do{
+        
+        System.out.println("Bem vindo!!!");
+        System.out.println("-------------");
+        System.out.println("1 - Criar empresa");
+        System.out.println("2 - Criar uma pessoa");
+        System.out.println("3 - Contratar pessoa");
+        System.out.println("4 - Demitir pessoa");
+        System.out.println("0 - Sair");
+
+        switch(opcao){
+            case 1:
+            break;
+            case 2:
+            switch{
+                case 1:
+                System.out.print("Funcionario possui carro? 1 - Sim 2 - nao");
+                break;
+                default:
+                System.out.println("opcao invalida");
+                break;
+            }
+
+
+            }
+            break;
+            case 3:
+            break;
+            case 4:
+            break;
+            case 0:
+            System.out.println("Opcao invalida");
+            break;
+            
+
+
+
+        }
+
+        }while (opcao =! 0)
 
         Empresa empresa1 = new Empresa();
         empresa1.setNome("Empresa Bancaria");
